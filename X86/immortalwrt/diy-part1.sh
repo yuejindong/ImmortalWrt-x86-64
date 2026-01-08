60598c35c23bbe8ae65af234b20f82deef77b995
yuejindong <89678004+yuejindong@users.noreply.github.com>
2026-01-09 06:57:08 +0800

    Update diy-part1.sh

--- X86/immortalwrt/diy-part1.sh
+++ X86/immortalwrt/diy-part1.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git' >>feeds.conf.default
+echo 'src-git immortalwrt https://github.com/whiskyrye/ImmortalWrt-Packages.git;main' >>feeds.conf.default
